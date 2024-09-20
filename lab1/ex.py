443fca7da7e03ea8e4dfac826422b31b17f06c1a
InOutLake <evpatcolovratez@gmail.com>
2024-09-20 13:45:56 +0700

    lab1 completed

---
+++ lab1/ex.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+# ex 1
+for w in 'Silence is golden'.split(' '):
+    print(w)
+
+# ex 2
+print(int(input()) % 2 == 0)
+
+# ex 3
+print(datetime.now().year - int(input()))
